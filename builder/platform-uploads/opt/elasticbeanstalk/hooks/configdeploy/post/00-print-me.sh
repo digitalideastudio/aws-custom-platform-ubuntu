4199437509924d91a1c2065ad26dc16c7fa13d6e
Serhii Matrunchyk <serhii@digitalidea.studio>
2017-09-29 02:21:17 +0300

    Fixed TA-188

--- builder/platform-uploads/opt/elasticbeanstalk/hooks/configdeploy/post/00-print-me.sh
+++ builder/platform-uploads/opt/elasticbeanstalk/hooks/configdeploy/post/00-print-me.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 echo "[Configuration Deployment] Executed when starting configuration deployment"
+
+. /etc/TecdonorPlatform/platform.config
+
+cd $LIVE_DIR
+chown www-data: -R ./
+chmod 777 storage/logs
+find public -type d -exec chmod 777 {} \;
